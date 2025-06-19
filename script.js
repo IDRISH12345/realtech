51c5a65d710ca735b761b62ee3392efea2ea60db
IDRISH12345 <77716935+IDRISH12345@users.noreply.github.com>
2025-06-19 11:58:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % totalSlides;
+  updateSlide();
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+  updateSlide();
+}
+
+function updateSlide() {
+  slides.style.transition = "transform 0.5s ease-in-out";
+  slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
